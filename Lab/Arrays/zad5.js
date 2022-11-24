88a43a982f72e4d88a5b05ed2606ceb9af71c0ce
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-24 19:32:57 +0200

    Update zad5.js

--- Lab/Arrays/zad5.js
+++ Lab/Arrays/zad5.js
@@ -1,13 +1,17 @@
 function solve(input) {
+    
     let numbers = 0;
     let sum = 0;
+    
     for (let i = 0; i < input.length; i++) {
+        
         numbers = Number(input[i]);
+        
         if (numbers % 2 == 0) {
             sum += numbers;
         }
     }
+    
     console.log(sum);
 
 }
-solve(['1', '2', '3', '4', '5', '6'])
