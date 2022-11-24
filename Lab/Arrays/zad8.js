be1dcb70f39eea709f08b327a1eb2abc3ea74d0f
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-24 19:35:36 +0200

    Update zad8.js

--- Lab/Arrays/zad8.js
+++ Lab/Arrays/zad8.js
@@ -1,12 +1,17 @@
 function solve(array) {
+    
     while (array.length > 1) {
+        
         let condensed = [];
+        
         for (let i = 0; i < array.length - 1; i++) {
+            
             condensed[i] = array[i] + array[i + 1];
+            
         }
         array = condensed;
     }
+    
     console.log(array[0]);
-
+    
 }
-solve([2, 10, 3]);
