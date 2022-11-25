fe468723db28a9313dc0e077a3b23b7ae6965257
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-25 09:34:27 +0200

    Update zad6.js

--- Lab/Data Types and Variables/zad6.js
+++ Lab/Data Types and Variables/zad6.js
@@ -2,7 +2,6 @@ function solve(band, album, song) {
 
     let time = (album.length * band.length) * song.length / 2;
     let result = Math.ceil(time / 2.5);
+    
     console.log(`The plate was rotated ${result} times.`);
-
 }
-solve('Black Sabbath', 'Paranoid', 'War Pigs')
