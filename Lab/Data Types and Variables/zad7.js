82f031b2e9e27860466b0bd17ea369b5129ce63a
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-25 09:34:46 +0200

    Update zad7.js

--- Lab/Data Types and Variables/zad7.js
+++ Lab/Data Types and Variables/zad7.js
@@ -2,7 +2,7 @@ function solve(pagesNumber, readIn1Hour, days) {
 
     let totalTimeToRead = pagesNumber / readIn1Hour;
     let hoursPerDay = totalTimeToRead / days;
+    
     console.log(hoursPerDay);
-
 }
-solve(212, 20, 2);
+
