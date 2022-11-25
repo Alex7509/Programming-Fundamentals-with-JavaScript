d2af4ee9f8b6f0f8f6cf15536066f1896cf85dc2
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-25 09:37:45 +0200

    Update zad3.js

--- Lab/Function/zad3.js
+++ Lab/Function/zad3.js
@@ -5,11 +5,7 @@ function repeatString(string, count) {
     for (let i = 0; i < count; i++) {
 
         result += string;
-
     }
 
     return result;
-
 }
-result = repeatString('abc', 3);
-console.log(result);
