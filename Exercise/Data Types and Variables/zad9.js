d32d58112f90321c5f9cc8e7716ac8a50f6c49ec
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-27 20:07:17 +0200

    Update zad9.js

--- Exercise/Data Types and Variables/zad9.js
+++ Exercise/Data Types and Variables/zad9.js
@@ -1,15 +1,19 @@
 function solve(lostFightsCount, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+    
     let result = 0;
     let brokenShield = 0;
 
     for (let i = 1; i <= lostFightsCount; i++) {
         let currentLostFights = i;
+        
         if (currentLostFights % 2 == 0) {
             result += helmetPrice;
         }
+        
         if (currentLostFights % 3 == 0) {
             result += swordPrice;
         }
+        
         if (currentLostFights % 2 == 0 && currentLostFights % 3 == 0) {
             result += shieldPrice;
             brokenShield++;
@@ -20,6 +24,4 @@ function solve(lostFightsCount, helmetPrice, swordPrice, shieldPrice, armorPrice
     }
 
     console.log(`Gladiator expenses: ${result.toFixed(2)} aureus`);
-
 }
-solve(23, 12.50, 21.50, 40, 200)
