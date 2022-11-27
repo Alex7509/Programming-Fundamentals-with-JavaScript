9d6b1dfba3b162a84c0845e98a44839dc2a3fa0d
Alex Georgiev <117475285+Alex7509@users.noreply.github.com>
2022-11-27 20:06:32 +0200

    Update zad8.js

--- Exercise/Data Types and Variables/zad8.js
+++ Exercise/Data Types and Variables/zad8.js
@@ -1,16 +1,16 @@
 function solve(number, operator, anotherNumber) {
 
     let result = 0;
+    
     if (operator === '+') {
         result = number + anotherNumber;
     } else if (operator === '-') {
-        result = number - anotherNumber
+        result = number - anotherNumber;
     } else if (operator === '*') {
-        result = number * anotherNumber
+        result = number * anotherNumber;
     } else if (operator === '/') {
-        result = number / anotherNumber
+        result = number / anotherNumber;
     }
+    
     console.log(result.toFixed(2));
-
 }
-solve(5, '+', 10)
